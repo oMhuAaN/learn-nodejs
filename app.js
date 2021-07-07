90fa3c160ec03d01e856598e8d471bab353d43d0
haibin@k590s-i7 <894457272@qq.com>
2021-07-07 19:42:30 +0800

    23224233

---
+++ app.js
@@ -0,0 +1,23 @@
+// var fs =require('fs');
+// var path='./wwwroot';
+// var dirArr=[];
+// fs.readdir(path,(err,data)=>{
+//   if(err){
+//     console.log(err)
+//   }
+//   (function getDir(i){
+//     if(i==data.length){
+//       console.log(dirArr)
+//       return
+//     }else{
+//       dirArr.push(i)
+//       getDir(i+1)
+//     }
+//   })(0)
+// })
+
+
+// 用递归的方式计算1到100的和
+function aa(){
+  
+}
